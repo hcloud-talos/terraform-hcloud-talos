dd1b34ad31c5dd45b51b6a1fdc1ae9a4849381ed
Marcel Richter <mail@mrclrchtr.de>
2024-03-18 02:18:35 +0100

    feat: Add hcloud-cloud-controller-manager

---
+++ manifest_hcloud_ccm.tf
@@ -0,0 +1,28 @@
+data "helm_template" "hcloud_ccm" {
+  name      = "hcloud-cloud-controller-manager"
+  namespace = "kube-system"
+
+  repository = "https://charts.hetzner.cloud"
+  chart      = "hcloud-cloud-controller-manager"
+  version    = "1.19.0"
+
+  set {
+    name  = "networking.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "networking.clusterCIDR"
+    value = local.pod_ipv4_cidr
+  }
+}
+
+data "kubectl_file_documents" "hcloud_ccm" {
+  content = data.helm_template.hcloud_ccm.manifest
+}
+
+resource "kubectl_manifest" "apply_hcloud_ccm" {
+  for_each   = data.kubectl_file_documents.hcloud_ccm.manifests
+  yaml_body  = each.value
+  depends_on = [data.talos_cluster_health.this]
+}
