d5e8242d74ed4b330f3865d2bb05ae73b9fcce80
Gytis Ivaskevicius <me@gytis.io>
2025-05-09 17:13:28 +0300

    feat: add cluster autoscaler

--- terraform.tf
+++ terraform.tf
@@ -38,6 +38,13 @@ provider "hcloud" {
   token = var.hcloud_token
 }
 
+provider "kubernetes" {
+  host                   = local.kubeconfig_data.host
+  cluster_ca_certificate = local.kubeconfig_data.cluster_ca_certificate
+  client_certificate     = local.kubeconfig_data.client_certificate
+  client_key             = local.kubeconfig_data.client_key
+}
+
 provider "helm" {
   kubernetes {
     host                   = local.kubeconfig_data.host
